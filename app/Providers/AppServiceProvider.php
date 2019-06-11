5a0c938dbea7a28e651cc195a9fd5df02e4ba56f
lulinyang <15655569098@163.com>
2019-06-11 13:01:56 +0800

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //手动配置迁移命令migrate生成的默认字符串长度
+        Schema::defaultStringLength(191);
     }
 
     /**
